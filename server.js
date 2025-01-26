0ebd5e331811fbeef9d459b35e683adcb0d77c2e
Rostislav <rostiksmoliar@email.com>
2025-01-26 13:37:43 +0200

    Ensure dynamic port binding

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ let lastResults = [];
 
 // Настраиваем порт (Render передаёт его через process.env.PORT)
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
